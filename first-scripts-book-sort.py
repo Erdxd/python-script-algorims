ba01e2c986586c6cff7d091b81051ca19bd4b45f
erdxd <tytalexsey@mail.ru>
2025-09-02 04:43:01 +0700

    add new algoritms on py and go (alg binary search)

--- first-scripts-book-sort.py
+++ first-scripts-book-sort.py
@@ -1,1 +1,16 @@
-print(2)
+def bin_search(arr,item): 
+    low = 0 
+    max = len(arr) - 1 
+
+    while low <= max:
+        mid = (low + max) // 2 
+        guess = arr[mid]
+        if guess == item:
+            return mid 
+        elif guess > mid:
+            max = mid -1 
+        else:
+            low = mid + 1 
+    return None
+my_list = [1,2,3,4,5,6]
+print(bin_search(my_list,2))
